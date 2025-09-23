de86690b339b02f9895d2017674a664973aebd25
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-09-23 17:38:13 +0200

    feat: ajouter des types d'exportation pour les actions, événements, fetch et prompt

--- packages/validate-openalex/src/index.ts
+++ packages/validate-openalex/src/index.ts
@@ -2,3 +2,8 @@ export * from './actions';
 export * from './prompt';
 export * from './store';
 export * from './events';
+export type * from './actions/types';
+export type * from './events/types';
+export type * from './fetch/types';
+export type * from './prompt/types';
+export type * from './store/types';
