2a8e769a3a4cbb94e472abfa29a3d59fb2a107d8
Pierre-Olivier Chasset <pierre-olivier@chasset.net>
2025-09-29 14:35:30 +0200

    feat: ajouter la gestion de l'état avec la classe Store pour le traitement des API

--- packages/fetch-openalex/src/store.ts
+++ packages/fetch-openalex/src/store.ts
@@ -1,10 +1,4 @@
-import { Effect, RateLimiter, Queue, Ref } from 'effect';
-import { FetchError, fetchOnePage, type Query } from '@univ-lehavre/biblio-fetch-one-api-page';
-
-interface FetchAPIOptions {
-  filter?: string;
-  search?: string;
-}
+import { Effect, Ref } from 'effect';
 
 interface APIResponse<T> {
   meta: {
@@ -80,42 +74,4 @@ class Store<T> {
   }
 }
 
-interface FetchAPIConfig {
-  userAgent: string;
-  rateLimit: RateLimiter.RateLimiter.Options;
-  apiURL: string;
-  endpoint: string;
-  fetchAPIOptions: FetchAPIOptions;
-  perPage: number;
-}
-
-const fetchAPIQueue = <T>(opts: FetchAPIConfig): Effect.Effect<Queue.Queue<T>, never, never> =>
-  Effect.scoped(
-    Effect.gen(function* () {
-      const url: URL = new URL(`${opts.apiURL}/${opts.endpoint}`);
-      const params: Query = { ...opts.fetchAPIOptions, perPage: opts.perPage };
-
-      const ratelimiter: RateLimiter.RateLimiter = yield* RateLimiter.make(opts.rateLimit);
-      const curriedFetch = (q: Query): Effect.Effect<APIResponse<T>, FetchError, never> =>
-        ratelimiter(fetchOnePage<APIResponse<T>>(url, q, opts.userAgent));
-
-      const queue: Queue.Queue<T> = yield* Queue.unbounded<T>();
-      const store: Store<T> = yield* Effect.andThen(Ref.make(initialState), s => new Store<T>(s));
-
-      const worker: Effect.Effect<void, FetchError, never> = Effect.gen(function* () {
-        while (yield* store.hasMorePages()) {
-          yield* store.incPage();
-          params.page = yield* store.page;
-          const response: APIResponse<T> = yield* curriedFetch(params);
-          queue.offerAll(response.results);
-          yield* store.addNewItems(response);
-        }
-      });
-
-      yield* Effect.fork(worker);
-
-      return queue;
-    }),
-  );
-
-export { fetchAPIQueue, Store, initialState };
+export { Store, initialState, type IState };
